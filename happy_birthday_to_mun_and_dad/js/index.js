080a4f1aeab636ee7041d54173133f70b0d0b6cf
shunjiu <50403746+shunjiu@users.noreply.github.com>
2021-10-08 21:46:48 +0800

    Update index.js

--- happy_birthday_to_mun_and_dad/js/index.js
+++ happy_birthday_to_mun_and_dad/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||顺九|祝|爸爸｜妈妈|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||顺九|祝|爸爸|妈妈|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
